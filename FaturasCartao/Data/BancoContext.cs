e86b0ae1a72a0a2d0f7119c1ee7d7905c07a2551
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-06-06 05:38:27 -0300

    Adicionar arquivos de projeto.

---
+++ FaturasCartao/Data/BancoContext.cs
@@ -0,0 +1,16 @@
+﻿using FaturasCartao.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaturasCartao.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+
+
+    }
+}
